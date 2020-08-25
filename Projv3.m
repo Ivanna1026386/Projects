31dfa28aa1057a06afea9424e92ade05c4c4009f
vivekbhargava23 <vivek.bhargava@uni-konstanz.de>
2020-08-25 13:55:43 +0200

    Updated with accuracy and AUC for first batch

---
+++ Projv3.m
@@ -0,0 +1,216 @@
+%{
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Assignment 8 - AEML
+Submitted by -
+ - Vivek Bhargava (01/1025699)
+ - Ivanna Savonik (01/1026386)
+
+No. of functions used -
+
+Other details -
+
+
+  Data description
+  (https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/)
+  ------------------------------------------------------------------
+
+  > 
+
+    Number of Instances
+    -------------------
+    class 1 -> 59
+    class 2 -> 71
+ 	
+ 
+    Number of Attributes - 30
+    -------------------------
+
+Classification Lasso
+
+LDA
+QDA
+Logistic Reg
+KNN
+GNB
+SVM
+
+Model Validation - 
+1. ROC - curves
+2. Error - 0-1 loss error
+
+Random - forest 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%}
+
+clear all;
+close all;
+clc
+
+dataset = readtable('wdbc.csv');
+[n,k] = size(dataset);
+
+X_all = table2array(dataset(:,3:k));
+classes = (dataset(:, 2));
+%
+indOfClassB = find(strcmp(classes.Var2, 'B')==1);
+indOfClassM = setdiff(1:n,indOfClassB)';
+X_B = table2array(dataset(indOfClassB,3:k));
+X_M = table2array(dataset(indOfClassM,3:k));
+
+rng('default')
+
+Y = strcmp(table2array(classes),'M'); % returns 1 if M 0 if B
+
+% Y = 1 or 'TRUE' ==> Malignant (Positive)
+% Y = 0 or 'FALSE' ==> Benign (Negative)
+[X_train, X_test, Y_train, Y_test] = splitSample(X_all,Y, 0.8, true);
+
+[X_train_norm, mu_X_train, sigma_X_train] = featureNormalize(X_train);
+% Now normalizing testing data using value from training standards
+X_test_norm = (X_test - mu_X_train)./sigma_X_train;
+
+
+
+%
+% Part 1-a) Training a LDA Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[LDAtrainedClassifier, LDA_Accuracy] = LDAtrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_LDA_train,score_LDA_train] = LDAtrainedClassifier.predictFcn(X_train_norm);
+[X2_LDA,Y2_LDA,T2_LDA,AUC2_LDA] = perfcurve(Y_train,score_LDA_train(:,2),1);
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_LDA,score_LDA] = LDAtrainedClassifier.predictFcn(X_test_norm);
+[X1_LDA,Y1_LDA,T1_LDA,AUC1_LDA] = perfcurve(Y_test,score_LDA(:,2),1);
+
+LDA_Accuracy_test = length(find(Y_test ==Y_fit_LDA))/length(Y_test)
+
+figure
+confLDA = confusionchart(Y_test,Y_fit_LDA);
+
+% Part 1-b) Training a QDA Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[QDAtrainedClassifier, QDA_Accuracy] = QDAtrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_QDA_train,score_QDA_train] = QDAtrainedClassifier.predictFcn(X_train_norm);
+[X2_QDA,Y2_QDA,T2_QDA,AUC2_QDA] = perfcurve(Y_train,score_QDA_train(:,2),1);
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_QDA,score_QDA] = QDAtrainedClassifier.predictFcn(X_test_norm);
+[X1_QDA,Y1_QDA,T1_QDA,AUC1_QDA] = perfcurve(Y_test,score_QDA(:,2),1);
+
+QDA_Accuracy_test = length(find(Y_test ==Y_fit_QDA))/length(Y_test)
+
+figure
+confQDA = confusionchart(Y_test,Y_fit_QDA)
+
+
+
+% Part 1-c) Training a GNB Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[GNBtrainedClassifier, GNB_Accuracy] = GNBtrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_GNB_train,score_GNB_train]= GNBtrainedClassifier.predictFcn(X_train_norm);
+[X2_GNB,Y2_GNB,T2_GNB,AUC2_GNB] = perfcurve(Y_train,score_GNB_train(:,2),1);
+
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_GNB,score_GNB]= GNBtrainedClassifier.predictFcn(X_test_norm);
+[X1_GNB,Y1_GNB,T1_GNB,AUC1_GNB] = perfcurve(Y_test,score_GNB(:,2),1);
+
+GNB_Accuracy_test = length(find(Y_test ==Y_fit_GNB))/length(Y_test)
+
+
+figure
+confGNB = confusionchart(Y_test,Y_fit_GNB)
+
+
+% Part 1-d) Training a Logistic Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[LogistictrainedClassifier, Logistic_Accuracy] = LogistictrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_Logistic_train,score_Logistic_train] = LogistictrainedClassifier.predictFcn(X_train_norm);
+[X2_Logistic,Y2_Logistic,T2_Logistic,AUC2_Logistic] = perfcurve(Y_train,score_Logistic_train(:,2),1);
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_Logistic,score_Logistic] = LogistictrainedClassifier.predictFcn(X_test_norm);
+[X1_Logistic,Y1_Logistic,T1_Logistic,AUC1_Logistic] = perfcurve(Y_test,score_Logistic(:,2),1);
+
+Logistic_Accuracy_test = length(find(Y_test == Y_fit_Logistic))/length(Y_test)
+
+figure
+confLogistic = confusionchart(Y_test,Y_fit_Logistic)
+
+
+
+% Part 1-e) Training a Linear SVM Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[Linear_SVMtrainedClassifier, Linear_SVM_Accuracy] = Linear_SVMtrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_Linear_SVM_train, score_Linear_SVM_train] = Linear_SVMtrainedClassifier.predictFcn(X_train_norm);
+[X2_Linear_SVM,Y2_Linear_SVM,T2_Linear_SVM,AUC2_Linear_SVM] = perfcurve(Y_train,score_Linear_SVM_train(:,2),1);
+
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_Linear_SVM, score_Linear_SVM] = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
+[X1_Linear_SVM,Y1_Linear_SVM,T1_Linear_SVM,AUC1_Linear_SVM] = perfcurve(Y_test,score_Linear_SVM(:,2),1);
+
+Linear_SVM_Accuracy_test = length(find(Y_test == Y_fit_Linear_SVM))/length(Y_test)
+
+figure
+conf_LinearSVM = confusionchart(Y_test,Y_fit_Linear_SVM)
+
+
+
+
+% Part 1-f) Training a Quadratic SVM Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[Quadratic_SVMtrainedClassifier, Quadratic_SVM_Accuracy] = Quadratic_SVMtrainClassifier(X_train_norm, Y_train);
+
+% Calculating AUC for trained data
+[Y_fit_Quadratic_SVM_train,score_Quadratic_SVM_train] = Linear_SVMtrainedClassifier.predictFcn(X_train_norm);
+[X2_Quadratic_SVM,Y2_Quadratic_SVM,T2_Quadratic_SVM,AUC2_Quadratic_SVM] = perfcurve(Y_train,score_Quadratic_SVM_train(:,2),1);
+
+
+% Calculation AUC and Accuracy for testing data
+[Y_fit_Quadratic_SVM,score_Quadratic_SVM] = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
+[X1_Quadratic_SVM,Y1_Quadratic_SVM,T1_Quadratic_SVM,AUC1_Quadratic_SVM] = perfcurve(Y_test,score_Quadratic_SVM(:,2),1);
+
+Quadratic_SVM_Accuracy_test = length(find(Y_test == Y_fit_Quadratic_SVM))/length(Y_test)
+
+
+figure
+confLinearSVM = confusionchart(Y_test,Y_fit_Quadratic_SVM)
+
+
+
+%-----------------------------------------------------------------------
+%[X,Y,T,AUC] = perfcurve(double(Y_test),double(Y_fit_Quadratic_SVM),1)
+%-----------------------------------------------------------------------
+
+Accuracy_Training = [LDA_Accuracy,QDA_Accuracy,GNB_Accuracy, Logistic_Accuracy,Linear_SVM_Accuracy,Quadratic_SVM_Accuracy]
+Accuracy_Testing = [LDA_Accuracy_test, QDA_Accuracy_test, GNB_Accuracy_test, Logistic_Accuracy_test, Linear_SVM_Accuracy_test, Quadratic_SVM_Accuracy_test]
+
+
+
+AUC2_Training_data = [AUC2_LDA, AUC2_QDA, AUC2_GNB, AUC2_Logistic, AUC2_Linear_SVM, AUC2_Quadratic_SVM]
+AUC1_Testing_data = [AUC1_LDA, AUC1_QDA, AUC1_GNB, AUC1_Logistic, AUC1_Linear_SVM, AUC1_Quadratic_SVM]
+
+%}
+%}
+%}
+%}
+%}
+
+
+
+
+
+
+
--- Rough.m
+++ Rough.m
@@ -56,16 +56,17 @@ dataset = readtable('wdbc.csv');
 X_all = table2array(dataset(:,3:k));
 classes = (dataset(:, 2));
 %
-indOfClassB = find(strcmp(classes.Var2, 'B')==1)
-indOfClassM = setdiff(1:n,indOfClassB)'
-X_B = table2array(dataset(indOfClassB,3:k))
-X_M = table2array(dataset(indOfClassM,3:k))
+indOfClassB = find(strcmp(classes.Var2, 'B')==1);
+indOfClassM = setdiff(1:n,indOfClassB)';
+X_B = table2array(dataset(indOfClassB,3:k));
+X_M = table2array(dataset(indOfClassM,3:k));
 
+rng('default')
 
+Y = strcmp(table2array(classes),'M'); % returns 1 if M 0 if B
 
-Y = strcmp(table2array(classes),'M') % returns 1 if M 0 if B
-% Y = 1 or 'TRUE' ==> Malignant
-% Y = 0 or 'FALSE' ==> Benign
+% Y = 1 or 'TRUE' ==> Malignant (Positive)
+% Y = 0 or 'FALSE' ==> Benign (Negative)
 [X_train, X_test, Y_train, Y_test] = splitSample(X_all,Y, 0.8, true);
 
 [X_train_norm, mu_X_train, sigma_X_train] = featureNormalize(X_train);
@@ -73,58 +74,85 @@ Y = strcmp(table2array(classes),'M') % returns 1 if M 0 if B
 X_test_norm = (X_test - mu_X_train)./sigma_X_train;
 
 
+
+%
 % Part 1-a) Training a LDA Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [LDAtrainedClassifier, LDA_Accuracy] = LDAtrainClassifier(X_train_norm, Y_train);
-Y_fit_LDA = LDAtrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_LDA,score_LDA] = LDAtrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_LDA = confusionchart(Y_test,Y_fit_LDA)
+confLDA = confusionchart(Y_test,Y_fit_LDA);
+find(Y_fit_LDA ==Y_fit_LDA)
+
+[X1_LDA,Y1_LDA,T1_LDA,AUC1_LDA] = perfcurve(Y_test,score_LDA(:,2),1)
+
+% Calculating AUC for trained data
+[Y_fit_LDA_train,score_LDA_train] = LDAtrainedClassifier.predictFcn(X_train_norm);
+[X2_LDA,Y2_LDA,T2_LDA,AUC2_LDA] = perfcurve(Y_train,score_LDA_train(:,2),1);
+
 
 % Part 1-b) Training a QDA Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [QDAtrainedClassifier, QDA_Accuracy] = QDAtrainClassifier(X_train_norm, Y_train);
-Y_fit_QDA = QDAtrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_QDA,score_QDA] = QDAtrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_QDA = confusionchart(Y_test,Y_fit_QDA)
+confQDA = confusionchart(Y_test,Y_fit_QDA)
+
+[X1_QDA,Y1_QDA,T1_QDA,AUC1_QDA] = perfcurve(Y_test,score_QDA(:,2),1)
 
 % Part 1-c) Training a GNB Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [GNBtrainedClassifier, GNB_Accuracy] = GNBtrainClassifier(X_train_norm, Y_train);
-Y_fit_GNB = GNBtrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_GNB,score_GNB]= GNBtrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_GNB = confusionchart(Y_test,Y_fit_GNB)
+confGNB = confusionchart(Y_test,Y_fit_GNB)
+
+[X1_GNB,Y1_GNB,T1_GNB,AUC1_GNB] = perfcurve(Y_test,score_GNB(:,2),1)
 
 
 % Part 1-d) Training a Logistic Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [LogistictrainedClassifier, Logistic_Accuracy] = LogistictrainClassifier(X_train_norm, Y_train);
-Y_fit_Logistic = LogistictrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_Logistic,score_Logistic] = LogistictrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_Logistic = confusionchart(Y_test,Y_fit_Logistic)
+confLogistic = confusionchart(Y_test,Y_fit_Logistic)
+
+[X1_Logistic,Y1_Logistic,T1_Logistic,AUC1_Logistic] = perfcurve(Y_test,score_Logistic(:,2),1)
 
 
 % Part 1-e) Training a Linear SVM Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [Linear_SVMtrainedClassifier, Linear_SVM_Accuracy] = Linear_SVMtrainClassifier(X_train_norm, Y_train);
-Y_fit_Linear_SVM = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_Linear_SVM, score_Linear_SVM] = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_Linear_SVM = confusionchart(Y_test,Y_fit_Linear_SVM)
+conf_LinearSVM = confusionchart(Y_test,Y_fit_Linear_SVM)
+
+[X1_Linear_SVM,Y1_Linear_SVM,T1_Linear_SVM,AUC1_Linear_SVM] = perfcurve(Y_test,score_Linear_SVM(:,2),1)
 
 
 % Part 1-f) Training a Quadratic SVM Model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [Quadratic_SVMtrainedClassifier, Quadratic_SVM_Accuracy] = Quadratic_SVMtrainClassifier(X_train_norm, Y_train);
-Y_fit_Quadratic_SVM = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
+[Y_fit_Quadratic_SVM,score_Quadratic_SVM] = Linear_SVMtrainedClassifier.predictFcn(X_test_norm);
 figure
-conf_Quadratic_SVM = confusionchart(Y_test,Y_fit_Quadratic_SVM)
+confLinearSVM = confusionchart(Y_test,Y_fit_Quadratic_SVM)
+
+[X1_Quadratic_SVM,Y1_Quadratic_SVM,T1_Quadratic_SVM,AUC1_Quadratic_SVM] = perfcurve(Y_test,score_Quadratic_SVM(:,2),1)
+
+%-----------------------------------------------------------------------
+%[X,Y,T,AUC] = perfcurve(double(Y_test),double(Y_fit_Quadratic_SVM),1)
+%-----------------------------------------------------------------------
+
+Accuracy_Training = [LDA_Accuracy,QDA_Accuracy,GNB_Accuracy, Logistic_Accuracy,Linear_SVM_Accuracy,Quadratic_SVM_Accuracy]
+
+AUC1_Testing_data = [AUC1_LDA, AUC1_QDA, AUC1_GNB, AUC1_Logistic, AUC1_Linear_SVM, AUC1_Quadratic_SVM]
 
 
-
-
-
-
-
-
+%}
+%}
+%}
+%}
+%}
 
 
 
